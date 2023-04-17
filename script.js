7eb9a4e21cb873cdc158c281039bd286f0a3fdc8
shrekTheLegend69 <shrekthesaviour69@gmail.com>
2023-04-17 20:21:10 +0200

    Rework the code so it makes 16 parent divs with 16 children to make grid layout possible using flexbox

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// Create 16x16 divs with class .grid inside the container div
+// Create 16 divs nesting another 16 divs
 const container = document.querySelector('#container');
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.classList.add('grid');
-    container.appendChild(gridDiv);
+for (let i = 0; i < 16; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    container.appendChild(rowDiv);
+        for (let j = 0; j < 16; j++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.classList.add('grid');
+        rowDiv.appendChild(gridDiv); 
+        }
 }
-
 
