338de0886a3a9b31638b40cbc577a53c7d90b452
shrekTheLegend69 <shrekthesaviour69@gmail.com>
2023-04-18 14:02:01 +0200

    Add feat that gives user a choice to decide grid size

--- script.js
+++ script.js
@@ -13,11 +13,45 @@ for (let i = 0; i < 16; i++) {
 }
 
 // Turn all grid divs black on mouseover
-const pixels = document.querySelectorAll('.grid');
+let pixels = document.querySelectorAll('.grid');
 
 pixels.forEach(pixel => {
     pixel.addEventListener('mouseover', function(e) {
+        console.log('works');
         pixel.style.backgroundColor = "black";
     })
 });
 
+// Change grid to X by X size (X is decided by user input)
+const gridButton = document.querySelector('.gridSize');
+
+gridButton.addEventListener('click', function(e) {
+    let size = prompt("Please enter the new grid size (maximum 100):");
+    buildGrid(size);
+});
+
+function buildGrid(size) {
+    removeElements('row');
+    for (let i = 0; i < size; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        container.appendChild(rowDiv);
+            for (let j = 0; j < size; j++) {
+            const gridDiv = document.createElement('div');
+            gridDiv.classList.add('grid');
+            rowDiv.appendChild(gridDiv); 
+            }
+    }
+    pixels = document.querySelectorAll('.grid');
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', function(e) {
+            console.log('works');
+            pixel.style.backgroundColor = "black";
+        })
+    });
+}
+
+function removeElements(name) {
+    const elements = document.querySelectorAll('.'+ name);
+    elements.forEach(element => element.remove());
+}
